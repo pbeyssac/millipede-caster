0a0498f626ab14af2822dd21be51d5d7222d87bb
Pierre Beyssac <pb@fasterix.frmug.org>
2024-08-16 22:17:11 +0200

    Fix Linux compile.

--- caster/queue.h
+++ caster/queue.h
@@ -21,4 +21,23 @@
 	    (var) = (tvar))
 #endif
 
+#ifndef QUEUE_TYPEOF
+#define	QUEUE_TYPEOF(type) struct type
+#endif
+
+#ifndef STAILQ_SWAP
+#define STAILQ_SWAP(head1, head2, type) do {				\
+	QUEUE_TYPEOF(type) *swap_first = STAILQ_FIRST(head1);		\
+	QUEUE_TYPEOF(type) **swap_last = (head1)->stqh_last;		\
+	STAILQ_FIRST(head1) = STAILQ_FIRST(head2);			\
+	(head1)->stqh_last = (head2)->stqh_last;			\
+	STAILQ_FIRST(head2) = swap_first;				\
+	(head2)->stqh_last = swap_last;					\
+	if (STAILQ_EMPTY(head1))					\
+		(head1)->stqh_last = &STAILQ_FIRST(head1);		\
+	if (STAILQ_EMPTY(head2))					\
+		(head2)->stqh_last = &STAILQ_FIRST(head2);		\
+} while (0)
+#endif
+
 #endif
