25ef3cab0707d73c8230091f123857cf3fe52e6d
Pierre Beyssac <pb@fasterix.frmug.org>
2024-08-23 00:38:24 +0200

    Fix compilation.

--- caster/util.c
+++ caster/util.c
@@ -643,7 +643,7 @@ char *malloc_stats_dump(int json) {
 #else
 
 char *malloc_stats_dump(int json) {
-	return mystrdup("{\"err\": \""no malloc stats available\""}");
+	return mystrdup("{\"err\": \"no malloc stats available\"}");
 }
 
 #endif
