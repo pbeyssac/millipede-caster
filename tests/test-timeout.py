bd9872c441661d96d411ad5d251d83009b1e9096
Pierre Beyssac <pb@fasterix.frmug.org>
2025-10-17 19:28:21 +0200

    Refactorize test code in a new module.

--- tests/test-timeout.py
+++ tests/test-timeout.py
@@ -1,12 +1,12 @@
 #!/usr/local/bin/python3
 
-import socket
 import sys
-import threading
-import time
+
+import testlib
 
 #
-# Client timeout test
+# Client timeout test:
+# Check the client connection is not closed when it doesn't send anything to the server
 #
 
 HOST='::1'
@@ -14,57 +14,19 @@ PORT=2103
 
 err = 0
 
-stop_source = False
-source_ok = False
+source_stream = testlib.SourceStream((HOST, PORT), "TEST1", "test1:testpw!", 200)
+source_stream.start()
 
-def source_stream():
-  global source_ok
-  global stop_source
-  ssource = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-  ssource.connect((HOST, PORT))
-  ssource.sendall(b'POST /TEST1 HTTP/1.1\nUser-Agent: NTRIP test\nAuthorization: Basic dGVzdDE6dGVzdHB3IQ==\n\n')
-  sdata = ssource.recv(10240)
-  source_ok = True
-  for i in range(200):
-    if stop_source:
-      break
-    ssource.sendall(b'%d\n' % i)
-    time.sleep(1)
-  ssource.close()
+client_stream = testlib.ClientStream((HOST, PORT), "TEST1", 30)
+client_stream.start()
+client_stream.join(120)
+source_stream.stop()
 
-def client_stream():
-  global stop_source
-  global err
-  sclient = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-  sclient.connect((HOST, PORT))
-  sclient.sendall(b'GET /TEST1 HTTP/1.1\r\nUser-Agent: NTRIP test\r\n\r\n')
-
-  for i in range(30):
-    data = sclient.recv(10240)
-    if data == b'':
-      print("FAIL: unexpected stop")
-      err = 1
-      break
-    print(".", end='')
-    sys.stdout.flush()
-  print()
-  stop_source = True
-  sclient.close()
-
-thr1 = threading.Thread(target=source_stream, daemon=True, args=())
-thr1.start()
-
-# Give time for the source to start
-while not source_ok and thr1.is_alive():
-  time.sleep(.1)
-
-thr2 = threading.Thread(target=client_stream, daemon=True, args=())
-thr2.start()
-
-thr2.join(120)
-if thr2.is_alive():
-  err = 1
+if client_stream.is_alive():
+  err += client_stream.err + 1
   print("FAIL: timeout")
+else:
+  err += client_stream.err
 
 print()
 sys.exit(err)
---
+++ tests/testlib.py
@@ -0,0 +1,76 @@
+#!/usr/local/bin/python3
+
+import base64
+import socket
+import sys
+import threading
+import time
+
+
+#
+# Source stream to server on a given mountpoint, user+password, number of samples
+#
+class SourceStream(object):
+  def __init__(self, host, mountpoint, userpass, n):
+    self._stop = False
+    self._ok = False
+    self.host = host
+    self.mountpoint = mountpoint
+    self.b64userpass = base64.b64encode(userpass.encode('ascii'))
+    self.n = n
+  def start(self):
+    self._thr = threading.Thread(target=self._run, daemon=True, args=())
+    self._thr.start()
+    # Give time for the source to start
+    while not self._ok and self.is_alive():
+      time.sleep(.1)
+  def _run(self):
+    ssource = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    ssource.connect(self.host)
+    ssource.sendall(b'POST /%s HTTP/1.1\nUser-Agent: NTRIP test\nAuthorization: Basic %s\n\n'
+        % (self.mountpoint.encode('ascii'), self.b64userpass))
+    sdata = ssource.recv(10240)
+    self._ok = True
+    for i in range(self.n):
+      if self._stop:
+        break
+      ssource.sendall(b'%d\n' % i)
+      time.sleep(1)
+    ssource.close()
+  def stop(self):
+    self._stop = True
+  def is_alive(self):
+    return self._thr.is_alive()
+
+#
+# Client stream to server on a given mountpoint: wait for n samples
+#
+class ClientStream(object):
+  def __init__(self, host, mountpoint, n):
+    self.host = host
+    self.n = n
+    self.mountpoint = mountpoint
+    self.err = 0
+  def start(self):
+    self._thr = threading.Thread(target=self._run, daemon=True, args=())
+    self._thr.start()
+  def _run(self):
+    sclient = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sclient.connect(self.host)
+    sclient.sendall(b'GET /%s HTTP/1.1\r\nUser-Agent: NTRIP test\r\n\r\n'
+        % self.mountpoint.encode('ascii'))
+
+    for i in range(self.n):
+      data = sclient.recv(10240)
+      if data == b'':
+        print("FAIL: unexpected stop")
+        self.err = 1
+        break
+      print(".", end='')
+      sys.stdout.flush()
+    print()
+    sclient.close()
+  def is_alive(self):
+    return self._thr.is_alive()
+  def join(self, timeout):
+    self._thr.join(timeout)
