f102ca20c7557975728e0d064155dc4cb9bd1973
Pierre Beyssac <pb@fasterix.frmug.org>
2025-10-17 17:53:16 +0200

    Refactorize test module.

--- tests/test-timeout.py
+++ tests/test-timeout.py
@@ -32,29 +32,39 @@ def source_stream():
     time.sleep(1)
   ssource.close()
 
-threading.Thread(target=source_stream, args=()).start()
+def client_stream():
+  global stop_source
+  global err
+  sclient = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+  sclient.connect((HOST, PORT))
+  sclient.sendall(b'GET /TEST1 HTTP/1.1\r\nUser-Agent: NTRIP test\r\n\r\n')
+
+  for i in range(30):
+    data = sclient.recv(10240)
+    if data == b'':
+      print("FAIL: unexpected stop")
+      err = 1
+      break
+    print(".", end='')
+    sys.stdout.flush()
+  print()
+  stop_source = True
+  sclient.close()
+
+thr1 = threading.Thread(target=source_stream, daemon=True, args=())
+thr1.start()
 
 # Give time for the source to start
-while not source_ok:
+while not source_ok and thr1.is_alive():
   time.sleep(.1)
 
-sclient = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-sclient.connect((HOST, PORT))
-sclient.sendall(b'GET /TEST1 HTTP/1.1\r\nUser-Agent: NTRIP test\r\n\r\n')
+thr2 = threading.Thread(target=client_stream, daemon=True, args=())
+thr2.start()
 
-for i in range(30):
-  data = sclient.recv(10240)
-  if data == b'':
-    print("FAIL: unexpected stop")
-    err = 1
-    break
-  print(".", end='')
-  sys.stdout.flush()
-print()
-
-stop_source = True
-
-sclient.close()
+thr2.join(120)
+if thr2.is_alive():
+  err = 1
+  print("FAIL: timeout")
 
 print()
 sys.exit(err)
