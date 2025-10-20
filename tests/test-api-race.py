563e674076e74f54c62dadc6ace43e25bb343f1a
Pierre Beyssac <pb@fasterix.frmug.org>
2025-10-20 12:40:00 +0200

    Update test-api-race.py.

--- tests/test-api-race.py
+++ tests/test-api-race.py
@@ -7,39 +7,50 @@ import time
 HOST='::1'
 PORT=2103
 
+N=100
+
 #
 # Test for race condition on API calls
 #
 err = 0
 for req in [
-  b'POST /adm/api/v1/sync HTTP/1.1\r\nContent-Length: 11\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\nContent-Type: application/json\r\n\r\n{"a":null}\n',
+  b'POST /adm/api/v1/reload HTTP/1.1\r\nContent-Length: 33\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nuser=admin&password=%3dadminpw...',
   b'GET /adm/api/v1/net?user=admin&password=%3dadminpw... HTTP/1.1\r\nContent-Length: 0\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\n\r\n',
+  b'POST /adm/api/v1/sync HTTP/1.1\r\nContent-Length: 11\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\nContent-Type: application/json\r\n\r\n{"a":null}\n',
   b'GET /adm/api/v1/rtcm?user=admin&password=%3dadminpw... HTTP/1.1\r\nContent-Length: 0\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\n\r\n'
   b'GET /adm/api/v1/mem?user=admin&password=%3dadminpw... HTTP/1.1\r\nContent-Length: 0\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\n\r\n'
   b'GET /adm/api/v1/livesources?user=admin&password=%3dadminpw... HTTP/1.1\r\nContent-Length: 0\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\n\r\n'
   b'GET /adm/api/v1/sourcetables?user=admin&password=%3dadminpw... HTTP/1.1\r\nContent-Length: 0\r\nAuthorization: internal 587e5bbadbc6186fad0d6177eb10a6cd9d5cb934d3d5f155107592535bd20290\r\n\r\n'
-  b'POST /adm/api/v1/reload HTTP/1.1\r\nContent-Length: 33\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nuser=admin&password=%3dadminpw...',
   b'POST /adm/api/v1/drop HTTP/1.1\r\nContent-Length: 33\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nuser=admin&password=%3dadminpw...',
   ]:
 
-  for i in range(100):
-    sclient = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    try:
-      sclient.connect((HOST, PORT))
-      sclient.sendall(req)
-      sclient.close()
 
-    except BrokenPipeError as e:
-      err = 1
-      print(e)
-      break
+  sockets = [socket.socket(socket.AF_INET6, socket.SOCK_STREAM) for i in range(N)]
+
+  try:
+    [s.connect((HOST, PORT)) for s in sockets]
+  except ConnectionResetError as e:
+    err = 1
+    print(e)
+  except BrokenPipeError as e:
+    err = 1
+    print(e)
+
+  if not err:
+    try:
+      [s.sendall(req) for s in sockets]
     except ConnectionResetError as e:
       err = 1
       print(e)
-      break
+    except BrokenPipeError as e:
+      err = 1
+      print(e)
 
-    print('.', end='')
-    sys.stdout.flush()
+  [s.close() for s in sockets]
+
+  print('.', end='')
+  sys.stdout.flush()
+  time.sleep(.01)
 
 if err:
   print("FAILED")
